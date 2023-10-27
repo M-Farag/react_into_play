8418aa640d129b25d5537801a62d64fb47e3ac91
M-Farag <mina.farag@icloud.com>
2023-10-27 14:58:07 +0200

    UPD EsLint
    UPD SearchParams to implement ReactHooks

--- nameCards/src/SearchParams.jsx
+++ nameCards/src/SearchParams.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
+
 const SearchParams = () =>
 {
-    const value = '';
+    const [value, setValue] = useState("");
 
     return (
         <div>
             <form>
                 <label htmlFor="names">
-                    <input id="names" value={value} placeholder="Search names" />
+                    <input id="names" value={value} placeholder="Search names" 
+                    onChange={(e)=>setValue(e.target.value)}
+                    />
                 </label>
                 <button>Submit</button>
             </form>
