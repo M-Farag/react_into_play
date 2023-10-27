bcd8b62ce2c8daee95834c7550790c0a54934edb
M-Farag <mina.farag@icloud.com>
2023-10-27 15:29:25 +0200

    ADD Departements list
    ADD Hook to select the department
    UPD EsLint

--- nameCards/src/App.jsx
+++ nameCards/src/App.jsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import SearchParams from "./searchParams";
 
 const App = () => {
--- nameCards/src/SearchParams.jsx
+++ nameCards/src/SearchParams.jsx
@@ -3,15 +3,40 @@ import { useState } from "react";
 const SearchParams = () =>
 {
     const [value, setValue] = useState("");
+    const [department,setDepartment] = useState("");
+
+    const departments = ["HR","Accounting","IT","Engineering"];
 
     return (
         <div>
-            <form>
-                <label htmlFor="names">
-                    <input id="names" value={value} placeholder="Search names" 
+            <form className="search-params">
+                <label htmlFor="name"> name:
+                    <input id="name" value={value} placeholder="Search name" 
                     onChange={(e)=>setValue(e.target.value)}
                     />
                 </label>
+                <br />
+                <label htmlFor="department"> Department 
+                    <select id="department"
+                    onChange={(e)=>{
+                        setDepartment(e.target.value)
+                    }}>
+                    
+                    <option />
+
+                    {
+                        departments.map(
+                            (dep) => 
+                                (
+                                <option key={dep}>{dep}</option>
+                                )
+                        )
+                    }
+                        
+                    </select>
+                </label>
+
+
                 <button>Submit</button>
             </form>
         </div>
