6e6e54f5f527a25cd5f904b384145826218e20c7
M-Farag <mina.farag@icloud.com>
2023-10-30 16:13:12 +0200

    UPD SearchParams to have dynamic linking to an animal

--- adoptMe/src/SearchParams.jsx
+++ adoptMe/src/SearchParams.jsx
@@ -2,15 +2,44 @@ import {useState} from "react";
 
 const SearchParams = () => {
     const [location, setLocation] = useState("");
+    const [animal,setAnimal] = useState("");
+    const animals = ['dog','cat','rabbit','bird'];
+
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">Location
                     <input onChange={(e) => setLocation(e.target.value)}  id="location" value={location} placeholder="Location"/>
                 </label>
+
+                <label htmlFor="animal">
+                    <select id="animal"
+                    onChange={(e)=>{
+                        setAnimal(e.target.value)
+                    }}
+                    >
+                        <option key="">Please select animal </option>
+                        {
+                            animals.map(
+                                (animal) => (
+                                    <option key={animal}>{animal}</option>
+                                )
+                            )
+                        }
+
+                    </select>
+                </label>
+
+
                 <button>Submit</button>
                 
             </form>
+
+            <p>
+                Search location is: {location}
+                <br />
+                Selected animal is: {animal}
+            </p>
         </div>
     );
 }
