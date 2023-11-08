fd308f2b99d38e881ccdc7f6eae661d2fe25ad17
M-Farag <mina.farag@icloud.com>
2023-11-08 20:48:18 +0200

    ADD Base component and styles

--- countriesAndCities/src/App.jsx
+++ countriesAndCities/src/App.jsx
@@ -1,8 +1,46 @@
+import { useEffect, useState } from "react";
 import ReactDOM  from "react-dom/client";
 
 const App = () => {
+
+    const [countries,setCountries] = useState([]);
+
+    // initially get countries
+    useEffect(
+        ()=> {
+
+            
+
+
+
+
+        },[]
+    );
+
     return (
-        <h1> Hello from React </h1>
+        <div className="box">
+        <div className="label countries">
+            <label for="countries">Countries:</label>
+            <select id="countries">
+            <option value="">--</option>
+            {
+                countries.map(
+                    c => {
+                        return <option key={c.code}>{c.name}</option>
+                    }
+                )
+            }
+            
+            </select>
+        </div>
+        <div className="label regions">
+            <label for="regions">Regions:</label>
+            <select id="regions">
+            <option value="region1">Region 1</option>
+            <option value="region2">Region 2</option>
+            </select>
+        </div>
+        </div>
     );
 }
 
