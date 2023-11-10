4f8cda9f620afb82251545bc964c5856317e63ec
M-Farag <mina.farag@icloud.com>
2023-11-10 22:01:37 +0200

    UPD Initial component

---
+++ productsDetails/src/App.jsx
@@ -0,0 +1,11 @@
+import  ReactDOM  from "react-dom/client";
+
+const App = () => {
+    return (
+        <h1>Hello from React</h1>
+    )
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
