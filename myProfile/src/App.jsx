340d28ad2db4a7664c44f5001aef5de30316cab3
M-Farag <mina.farag@icloud.com>
2023-11-11 23:36:56 +0200

    ADD Init MyProfile

---
+++ myProfile/src/App.jsx
@@ -0,0 +1,10 @@
+import  ReactDOM  from "react-dom/client"
+const App = () => {
+    return (
+        <h1>Hello React</h1>
+    )
+}
+
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+root.render(<App />);
