32c8c15c8377d66da74e792cf720ab2d8746f5a9
M-Farag <mina.farag@icloud.com>
2023-11-16 01:21:21 +0200

    UPD Contact form to be uncontrolled form

--- personalWebsite/src/pages/contact/Contact.jsx
+++ personalWebsite/src/pages/contact/Contact.jsx
@@ -1,9 +1,56 @@
+import { useState } from 'react';
+
 const Contact = () => {
+
+    const [requestParams, setRequestParams] = useState({
+        name: "",
+        email: "",
+        message: "",
+      });
+
+
     return (
         <div className="content">
             <h2>Contact page</h2>
+            <form onSubmit={(e)=>{
+                e.preventDefault();
+                const formData =  new FormData(e.target);
+                const obj = {
+                    name: formData.get("name") ?? '',
+                    email: formData.get("email") ?? '',
+                    message: formData.get("message") ?? '',
+                
+                }
+        
+                setRequestParams(obj);
+                console.log(requestParams);
+            }}
+            
+            >
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    required
+                />
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    required
+                />
+                <label htmlFor="message">Message:</label>
+                <textarea
+                    id="message"
+                    name='message'
+                    required
+                ></textarea>
+                <button type="submit">Submit</button>
+            </form>
         </div>
-    )
-}
+    );
+};
 
 export default Contact;
